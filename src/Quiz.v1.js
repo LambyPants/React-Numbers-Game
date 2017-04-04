f8ec8bb534bee352de9fae5fa9fbe56e871a4edc
Ryan Lambert <rllambert@rollins.edu>
2017-04-04 20:49:20 +0000

    finish project commit

---
+++ src/Quiz.v1.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import QuizOptions from './QuizOptions';
+class Quiz extends  Component {
+    constructor(props){
+        
+        super(props);
+        
+        let riddle = {
+            resultsArray: [8,9,10,11],
+            field1: 5,
+            field2: 5,
+            answer: 10
+            
+        };
+        
+        this.state={riddle: riddle}
+        this.renderOptions = this.renderOptions.bind(this);
+
+    }
+    renderOptions(){
+            return(
+    <div className="options">
+    {this.state.riddle.resultsArray.map((option,i) => 
+        <QuizOptions option={option} key={i} />
+    )}
+    
+     </div>
+                )
+        }  
+    
+   render() {
+      return (
+          <div className="quiz">
+          <div className="quiz-content">
+         <p className="question"> What is the Sum of <span className="text-info">5</span> and <span className="text-info">5</span>? </p>
+         {this.renderOptions()}
+
+          </div>
+          <div className="play-again">
+          <a className="button">Play Again? </a>
+          </div>
+          </div>
+          )
+   }
+}
+
+export default Quiz;
